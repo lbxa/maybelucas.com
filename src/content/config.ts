da6c76c3f18665c247ed9494f254763b6f0c8d0d
Lucas Barbosa <lucasbrsa@users.noreply.github.com>
2024-08-21 18:50:31 +1000

    uploading v2 skeleton

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    date: z.date()
+  })
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
