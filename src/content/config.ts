5391091806365b58140fad470f00691969b18e15
Lucas Barbosa <lucasbrsa@users.noreply.github.com>
2024-08-21 21:49:26 +1000

    new nav component; greatly simplified layout

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,6 +6,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     author: z.string(),
+    description: z.string(),
     date: z.date()
   })
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,9 +4,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		spacing: {
+			'sm': '8px',
+      'md': '12px',
+      'lg': '16px',
+      'xl': '24px',
+      '2xl': '32px',
+      '3xl': '48px',
+		}
 	},
 	plugins: [],
-	corePlugins: {
-		preflight: false // NOT RECOMMENDED
-	}
 } satisfies Config;
