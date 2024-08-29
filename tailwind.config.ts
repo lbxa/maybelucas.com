dcd91f0d552a8045405210d1f7c9282d11314900
Lucas Barbosa <lucasbrsa@users.noreply.github.com>
2024-08-29 16:21:54 +1000

    added dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,24 @@ import type { Config } from "tailwindcss";
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				shark: {
+					'50': '#f6f6f6',
+					'100': '#e7e7e7',
+					'200': '#d1d1d1',
+					'300': '#b0b0b0',
+					'400': '#888888',
+					'500': '#6d6d6d',
+					'600': '#5d5d5d',
+					'700': '#4f4f4f',
+					'800': '#454545',
+					'900': '#3d3d3d',
+					'950': '#121212',
+				},
+				ivory: '#f5f5f5'
+			},
+		},
 		spacing: {
 			'sm': '8px',
       'md': '12px',
@@ -13,5 +30,6 @@ export default {
       '3xl': '48px',
 		}
 	},
+	darkMode: "selector",
 	plugins: [],
 } satisfies Config;
